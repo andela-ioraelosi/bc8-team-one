362519d1ddb158720cad82bd79d39d8d9da1b68e
unknown <oatalabi@gmail.com>
2015-05-13 17:35:36 +0200

    feature.js

--- src/imoh/cream.js
+++ src/imoh/cream.js
@@ -1,1 +1,9 @@
-imoh
+var x = 49;
+
+if(x >= 70 && x <= 100) {
+	console.log(x+" is A")
+} elseif( x >= 50) {
+	console.log(x+" is B")
+} else if(x >= 0) {
+	console.log(x+" is C")
+}
---
+++ src/imoh/feature.js
@@ -0,0 +1,65 @@
+console.log("imoh")
+
+/*
+14 ---------- numbers
+-80 --------- numbers
+8.5 --------- numbers
+true -------- booleans
+(5 < 17) ---- booleans
+null -------- undefined
+3.142 ------- numbers
+"pi" -------- strings
+67+18 ------- function
+*/
+
+//convert number to base 2
+var base2converter = function(number) {
+	var i= parseInt(number, 10).toString(2);
+	console.log(i)
+
+}
+base2converter(60);
+
+
+var score= 180;
+printGrade= function(score) {
+
+	if ( (score <=100) && (score >= 0)) {
+		if(score >= 70) {
+			console.log(score+" is A")
+
+		} else if (score >= 50) {
+			console.log(score+" is B")
+
+		} else {console.log(score+" is C")}
+
+	}
+    else {
+    	console.log ("Enter Valid number")
+}
+}
+printGrade(100)
+
+
+var score= 180;
+
+
+FizzBuzz= function(number) {
+
+	if ( number % 3 == 0 && number % 5 == 0) {
+		console.log("FizzBuzz")
+	}
+    else if (number % 3 == 0)
+    {
+    	console.log ("Fizz")
+    }
+    else if ( (number %3 !== 0) && (number % 5 === 0) ) 
+    {
+    	console.log("Buzz")
+    }
+    else {
+    	console.log(number)
+    }
+};
+ 
+FizzBuzz(180)
