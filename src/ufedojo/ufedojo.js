ce3fc5b0929be5617c4ada8863c717da8825db55
Ufedojo Daniel Opaluwa <nikitude@gmail.com>
2015-05-13 18:06:14 +0100

    day3 activity and answers

--- src/ufedojo/ufedojo.js
+++ src/ufedojo/ufedojo.js
@@ -1,1 +1,48 @@
 console.log('Ufedojo D Opaluwa');
+/*
+Activity One (data types)
+==========================
+
+what data type:
+
+14 ------------------ number
+-80 ------------------ number
+8.5 ------------------ number
+true ------------------ boolean
+(5 < 17) ------------------ boolean
+null ------------------ undefined
+3.142 ------------------ number
+"Pi" ------------------ string
+67+18 ------------------ number
+*/
+
+/*****************************
+DECIMAL TO BINARY
+*****************************/
+var decToBin = function(decValue){
+	if(isNaN(decValue)){
+		console.log("Entry is not a number");
+	}
+	//notifies user that argument is not valid 
+	else {
+		var binValue = "";
+		var power = 0;
+
+		while(Math.pow(2,power)<decValue){
+			power++;
+		}
+		//above blocks find the nearest 2^n to decValue
+
+		while(power>=0){
+			if(decValue >= Math.pow(2,power)){
+				binValue+="1";
+			} else if(decValue<Math.pow(2,power)) {
+				binValue+="0";
+			}	
+			 decValue = decValue % Math.pow(2,power);
+			 power--;
+		}
+	 
+	 	}
+return binValue;
+};
