598b8562602910e56af7cc84e40cca5ce369178a
ThirdSamuel <awolumatesamuel.3sa@gmail.com>
2015-05-13 14:54:46 +0000

    I made a comment

--- src/samuel/feature.js
+++ src/samuel/feature.js
@@ -1,2 +1,13 @@
 console.log("Awolumate Samuel");
 console.log("I am ThirdSamuel");
+
+/*14 - number
+-80 - number
+8.5 - number
+true - boolean
+(5 < 7) - boolean
+null - unidentified
+3.142 - number
+"pi" - string
+67 + 18 - number
+*/
