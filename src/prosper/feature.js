8ceced08b72ec2ffafea92bef20cde5e2d04ee8b
smilopintus <prospy_711@yahoo.com>
2015-05-14 09:44:58 +0100

    did my corrections

--- src/prosper/feature.js
+++ src/prosper/feature.js
@@ -17,47 +17,57 @@ null --------------------- Undefined
 
 console.log("Prosper Oyibo");				//log my name to console
 
-convertToBinary = function(x) {
+var convertToBinary = function(decimal) {
 	//Converts decimal to binary
 	var result = [];						//Initialize result array
-	while (x) {
-		 var remainder = x % 2;				//get modulus of x
+	
+	while (decimal) {
+		 var remainder = decimal % 2;				//get modulus of decimal
 		 result.push(remainder);	 			//store the modulus in the result array
-		 x = (x / 2) | 0;						//get the new value for x
+		 decimal = (decimal / 2) | 0;						//get the new value for decimal
 	} 
+	
 	result.reverse();						// reverse the order of the array
 	return result.join("");					// join the elements of the array and return the result as string
 };
 
-countBs = function(x) {
-	if (typeof(x) === "string" ) {									//check for input type
-		for (var i = 0, len = x.length, count = 0; i < len; i++) {	//initialize variables
-			if (x.charAt(i) === "B") {								//check if the string contains 'B' character
+
+var countBs = function(word) {
+	
+	if (typeof(word) === "string" ) {									//check for input type
+		
+		for (var i = 0, len = word.length, count = 0; i < len; i++) {	//initialize variables
+			
+			if (word.charAt(i) === "B") {								//check if the string contains 'B' character
 				count++;											//increment count
 			}
 		}
 		return count;
+
 	} else {
 		return "Input is not a string";
 	}
 };
 
 
-countChar = function(x, y) {
-	if (typeof(x) === "string" && x.indexOf(y) !== -1) {			//check that both inputs are string and y is in x
-		for (var i = 0, max = x.length, count = 0; i < max; i++) {	//initialize variables
-			if (x.charAt(i) === y) {									
+var countChar = function(word, character) {
+	if (typeof(word) === "string" && word.indexOf(character) !== -1) {	//check that both inputs are string and character is in word
+
+		for (var i = 0, max = word.length, count = 0; i < max; i++) {	//initialize variables
+			
+			if (word.charAt(i) === character) {									
 				count++;											//increment count
 			}
+
 		}
 		return count;
+
 	} else {
 		return "Inputs not permitted";
 	}
 };
 
 
-countBs2 = function(x) {
-	return countChar(x, "B");										//call the function "countChar" with string "B" as the second input
+var countBs2 = function(word) {
+	return countChar(word, "B");										//call the function "countChar" with string "B" as the second input
 }
-
