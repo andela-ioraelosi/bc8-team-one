6446008f5ffad095dd78f6199822f2ef6d12b0ad
orezi <olarewajuore@gmail.com>
2015-05-14 11:55:41 +0100

    moved fibonacci activity to new folder

--- src/ore/feature.js
+++ src/ore/feature.js
@@ -65,12 +65,3 @@ function countBs(string)
 console.log(countBs("BABAbAbaB"));
 console.log(countChar("Bbazzzooookkkkka", "a"));
 
-//Fibonacci Series
-var fibonacci = function(n) {
-  var t = [];
-  for( var i=0; i < n; ++i){
-       t.push((i<2) ? i : t[i - 1] + t[i - 2]);
-       }
-      return t;
-    }
-console.log(fibonacci(70));
