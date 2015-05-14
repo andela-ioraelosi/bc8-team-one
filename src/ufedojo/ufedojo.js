7ee04d0cceffceb63027b949c0319ad78274b8d9
Ufedojo Daniel Opaluwa <nikitude@gmail.com>
2015-05-14 13:21:23 +0100

    day 4 exercises added

--- src/ufedojo/ufedojo.js
+++ src/ufedojo/ufedojo.js
@@ -75,3 +75,59 @@ return letterCount;
 var countBs = function(word){
 	return countChar(word,'B');
 }
+
+/*****************************
+FIBONACCI SEQUENCE
+*****************************/
+var fibonacci = function(seqLength) {
+	var sequence = [2,3];
+	if(seqLength == 1){
+		return sequence[0];
+	}
+	else if(seqLength == 2){
+		return sequence[2,3] ;
+	}
+	else{
+		for(var i = 3; i <= seqLength; i++){
+			var nextNum =(sequence[i-3] + sequence[i-2]);
+			sequence.push(nextNum);
+		}
+		return sequence.toString();
+	}
+}; 
+fibonacci(4);
+
+//REVISED FIBONACCI
+var fibonacci = function(seqLength) {
+	var a = b = 1;
+	var c = 0;
+	for (var i = 0; i < seqLength; i++){
+		c = a + b;
+		a = b;
+		b = c;
+		console.log(c);
+	}
+}; 
+fibonacci(4);
+
+
+
+/*****************************
+RECURSION factorial
+*****************************/	
+
+var factorial = function(N){
+	if(N == 0){
+		return 0;
+	}
+	else if(N == 1){
+		return 1;
+	}
+	else{
+		return N * factorial(N-1);
+	}
+};
+factorial(5);
+
+
+
