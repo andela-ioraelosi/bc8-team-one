7de05c2c5ee3b40bab78cf63ca8d106b5e26f666
Harod <harryachimugu@gmail.com>
2015-05-14 12:55:56 +0100

    add recursion activity

--- src/Harry/feature.js
+++ src/Harry/feature.js
@@ -12,17 +12,16 @@ true --------------- Boolean
 null ------------------ Undefined
 3.141 --------------- Number
 "Pi" ---------------- String
-67+18 --------------- Number 
-*/
+67+18 --------------- Number */
+
 
 //Activity two//
-var convertToBinary = function(x){
-
+var convertToBinary = function(decValue){
 var result = [];
-while (x){
-	var remainder = x % 2;
-	result.push(remainder)
-x= (x/2) | 0;
+	while (decValue){
+var remainder = decValue % 2;
+		result.push(remainder)
+		decValue= (decValue/2) | 0;
 }
 result.reverse()
 return result.join("");
@@ -33,35 +32,55 @@ console.log(convertToBinary(10));
 
 //Activity three//
 
-countsB = function(x) {
-	if (typeof(x) === "string" ) {
-		for (var i=0, max=x.length, count = 0; i<max; i++) {
-			if (x.charAt(i) === "B") {
-				count++;
-			}
-		}
-		return count;
-	} else {
-		return "input is not a string";
+/*var countsB = function(x) {
+	var counter = 0;
+		for (var i=0; i < x.length; i++) {
+			if (x.charAt(i) === "B") 
+				counter++;}
+return counter;
+}
+Console.log()
+
+var countChar = function(x, y) {
+var counter = 0;	
+	for (var i = 0, i < word.length; i++) {	
+		if (x.charAt(i) === y) {
+			counter++;}
+	}return counter;
+	}; 
+
+var countsB =function(word){
+	return countChar(word, 'B')
+}
+var seq = function (n)
+var result= [];
+for (var i= 0;j=1;k= 0; i < n; i=j;j=k;k++){
+    x=i+j;
+     result.push(x);
+}
+return result
+}
+   
+}
+console.log(seq(5));
+*/
+
+/*var fib= function(num){
+var (p=c=1, n=0);
+for (var i=0 i< num; i++){
+	n=p+c;p=c;c=n;console.log(n)
 	}
-};
+	fib(6)	)*/
 
-console.log(countsB("Boy"));
-
-
-
-countChar = function(x, y) {
-	if (typeof(x) === "string" && x.indexOf(y) !== -1) {
-		for (var i=0, max=x.length, count = 0; i<max; i++) {
-			if (x.charAt(i) === y) {
-	count++;
-		}
-		}
-	return count;
-	} else {
-		return "Please enter correct input";
-	}
-};
-
-console.log(countChar("BuBBle" , "Bath"));
+	
+//recursive exercise
+var factorial = function(numb) {
+    
+    if(i == numb); {
+        return numb{ 
+    else {
+    	      return numb *=i;
+    }
+}
+ 
 
