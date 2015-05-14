4a720b4a1f3b71426e577554ca939b6f64e241b4
Okamiye <kamiye@tinweb.com.ng>
2015-05-14 13:15:41 +0100

    Update: factorial function through recursion and create fibonacci.js file in my folder

--- src/kamiye/feature.js
+++ src/kamiye/feature.js
@@ -72,54 +72,11 @@ console.log(countBs('Bobo'));
 
 
 
-// Question: write a program to produce the sequence 2,3,5,8,13,21,34,...
 
-var numberOfSequence = function(n){
-	var sequenceArray = [2,3];
-	var newArrayElement;
-
-	var i = 1; // i is short for iterator
-	if(n > 2)
-	{
-		while(i <= n)
-		{
-			newArrayElement = sequenceArray[i] + sequenceArray[i-1]
-			// add the new element to the sequence array
-			sequenceArray.push(newArrayElement);
-			// increment iterator
-			i++;
-		}
-		return sequenceArray;
-	}
-	else
-	{
-		sequence = [];
-		for(var x = 0; x < n; x++)
-		{
-			sequence.push(sequenceArray[x]);
-		}
-		return sequence;
-	}
-	
+var factorial = function(num){
+	console.log(num);
+	if(num===0) return 1;
+	else return num*= factorial(num - 1);
 }
 
-console.log(numberOfSequence(8));
-
-// TOSIN'S SIMPLE SOLUTION
-
-var fibo = function(num){
-	var sequence = [];
-	var p = 1;
-	var c = 1;
-	var n;
-	for(var i = 0; i < num; i++ )
-	{
-		n = p+c;
-		p = c;
-		c = n;
-		sequence.push(n);
-	}
-	return sequence;
-}
-
-console.log(fibo(8));
+console.log(factorial(5));
---
+++ src/kamiye/fibonacci.js
@@ -0,0 +1,51 @@
+var numberOfSequence = function(n){
+	var sequenceArray = [2,3];
+	var newArrayElement;
+
+	var i = 1; // i is short for iterator
+	if(n > 2)
+	{
+		while(i <= n)
+		{
+			newArrayElement = sequenceArray[i] + sequenceArray[i-1]
+			// add the new element to the sequence array
+			sequenceArray.push(newArrayElement);
+			// increment iterator
+			i++;
+		}
+		return sequenceArray;
+	}
+	else
+	{
+		var sequence = [];
+		for(var x = 0; x < n; x++)
+		{
+			sequence.push(sequenceArray[x]);
+		}
+		return sequence;
+	}
+	
+}
+
+console.log(numberOfSequence(8));
+
+// Question: write a program to produce the sequence 2,3,5,8,13,21,34,...
+
+// TOSIN'S SIMPLE SOLUTION
+
+var fibo = function(num){
+	var sequence = [];
+	var p = 1;
+	var c = 1;
+	var n;
+	for(var i = 0; i < num; i++ )
+	{
+		n = p+c;
+		p = c;
+		c = n;
+		sequence.push(n);
+	}
+	return sequence;
+}
+
+console.log(fibo(8));
