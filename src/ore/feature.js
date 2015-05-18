43e9c11a8a7b9c63a1fbe5a0d7554cc250a3631b
orezi <olarewajuore@gmail.com>
2015-05-18 16:47:07 +0100

    create CacheFibonacciSeries.js, update index.html and feature.js

--- src/ore/feature.js
+++ src/ore/feature.js
@@ -65,4 +65,32 @@ function countBs(string)
 console.log(countBs("BABAbAbaB"));
 console.log(countChar("Bbazzzooookkkkka", "a"));
 
+var a=[];
+var sum = 0;
+var fibo = function(n){
+  //Base cases to build the sequence
+  sum = 0;
+  console.time('sum');
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+  if(n in a){
+    return a[n];
+  }
+  else {
+   return  a[n] = (fibo(n-1)+fibo(n-2));
+  
+  }
+}
+console.time("sum");
+for (var i= 0; sum < 4000000; i++){
+    var f = fibo(i);
+  if (f % 2 === 0) {
+    sum += f;
+    console.log("sum is: ", sum);
+  };
+}
+console.log(fibo(1000));
+console.timeEnd('sum');
+console.log(sum);
+
 
